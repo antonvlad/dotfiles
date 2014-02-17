a96afa76164e5203b0a487bb25a019249c300cb1
Anton Vladimirov <avladimirov@apple.com>
2014-02-17 14:49:21 -0800

    git-extras, homebrew, etc.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# First order of business - clone dotfiles and oh-my-zsh
+# Set the shell to Z shell
+chsh -s /usr/bin/zsh
+echo "Shell is set to $SHELL"
+
+# Clone dotfiles and oh-my-zsh
 echo "Cloning Dotfiles"
 git clone git://git@gitlab.sd.apple.com:avladimirov/dotfiles.git ~/.dotfiles
 
@@ -14,7 +18,16 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/anton-zsh-theme ~/.oh-my-zsh/themes/anton.zsh-theme
 
 # Install tools 
+echo "Installing git-up"
 sudo gem install git-up
+
+echo "Installing Homebrew"
+mkdir .homebrew && curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C .homebrew
+ln -s ~/.homebrew/bin/brew /usr/local/bin/brew
+
+echo "Installing git-extras"
+
+brew install git-extras
 
 # TODO: 
 # Pull source

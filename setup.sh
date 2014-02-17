7f7cd7d406d023305e328de2a439bbed9319adaa
Anton Vladimirov <avladimirov@apple.com>
2014-02-17 15:22:37 -0800

    Added some dir and link check. Cleaned up a bit

--- setup.sh
+++ setup.sh
@@ -1,34 +1,52 @@
 #!/bin/sh
+DOTFILES_DIR=~/.dotfiles
+OH_MY_ZSH_DIR=~/.oh-my-zsh
+HOMEBREW_DIR=~/.homebrew
 
 # Set the shell to Z shell
 chsh -s /usr/bin/zsh
 echo "Shell is set to $SHELL"
 
 # Clone dotfiles and oh-my-zsh
-echo "Cloning Dotfiles"
-git clone git://git@gitlab.sd.apple.com:avladimirov/dotfiles.git ~/.dotfiles
+if [ -d "$DOTFILES_DIR" ]; then
+    cd $DOTFILES_DIR && git pull --rebase
+else
+    echo "Cloning Dotfiles"
+    git clone git://git@gitlab.sd.apple.com:avladimirov/dotfiles.git $DOTFILES_DIR
+fi
 
-echo "Cloning Oh My Zsh"
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if [ -d "$OH_MY_ZSH_DIR" ]; then
+    cd $OH_MY_ZSH && git pull --rebase
+else
+    echo "Cloning Oh My Zsh"
+    git clone git://github.com/robbyrussell/oh-my-zsh.git $OH_MY_ZSH_DIR
+fi
 
 # Set up symlinks
 echo "Setting up symlinks"
 
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/anton-zsh-theme ~/.oh-my-zsh/themes/anton.zsh-theme
+if [ -e "~/.zshrc" ]; then 
+    rm ~/.zshrc
+fi
+ln -s $DOTFILES_DIR/.zshrc ~/.zshrc
+
+if [ -e $OH_MY_ZSH_DIR/themes/anton.zsh-theme ]; then
+    rm $OH_MY_ZSH_DIR/themes/anton.zsh-theme
+fi
+ln -s $DOTFILES_DIR/anton.zsh-theme $OH_MY_ZSH_DIR/themes/anton.zsh-theme
 
 # Install tools 
 echo "Installing git-up"
 sudo gem install git-up
 
 echo "Installing Homebrew"
-mkdir .homebrew && curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C .homebrew
+mkdir ~/.homebrew && curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C ~/.homebrew
 ln -s ~/.homebrew/bin/brew /usr/local/bin/brew
 
 echo "Installing git-extras"
 
-brew install git-extras
+/usr/local/bin/brew install git-extras
 
 # TODO: 
 # Pull source
-#  
+# git-radars

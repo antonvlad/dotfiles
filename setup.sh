a4bafa88916ec4bf5e6e7038ee9d3c3fce8e7932
Anton Vladimirov <avladimirov@apple.com>
2014-12-16 11:47:44 -0800

    modifying setup.sh to get the syntax highlighting plugin

--- setup.sh
+++ setup.sh
@@ -47,8 +47,21 @@ fi
 sudo ln -s ~/.homebrew/bin/brew /usr/local/bin/brew
 
 echo "Installing git-extras"
+cd /tmp 
+if [ -d "git-extras"]; then
+sudo rm -rf git-extras
+fi 
+
 $(cd /tmp && git clone --depth 1 https://github.com/visionmedia/git-extras.git && cd git-extras && sudo make install)
 
+echo "Installing zsh-syntax-highlighting"
+cd $OH_MY_ZSH_DIR/custom/plugins
+if [ -d "zsh-syntax-highlighting" ]; then
+    cd zsh-syntax-highlighting
+    sudo git pull --rebase
+else 
+    sudo git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+fi
 # TODO: 
 # Pull source
 # git-radars
